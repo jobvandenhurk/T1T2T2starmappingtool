c69cfd7cec6b7e05ef161595fbab23a409195c41
Job van den Hurk <jobvandenhurk@gmail.com>
2019-06-03 12:59:41 +0200

    Add files via upload

--- T1T2_saveresults.m
+++ T1T2_saveresults.m
@@ -1,6 +1,6 @@
-function T1T2_saveresults(dir,map,FitMap,fitparams,fun,usedxdata,data,est_par,TheseVoxels,x_label)
+function T1T2_saveresults(dir,map,FitMap,fitparams,fun,usedxdata,data,est_par,TheseVoxels,x_label,sliceselection)
 
-filename = [dir '/' est_par '_mappingresults_' date '.mat'];
+filename = [dir '/' est_par '_mappingresults_slice_' num2str(sliceselection) '_' date '.mat'];
 
 save(filename,'map','FitMap','fitparams','fun','usedxdata','data','est_par','TheseVoxels','x_label');
 disp(['Results saved to file ' filename]);
